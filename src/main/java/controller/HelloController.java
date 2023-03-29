987bf29f73d4b0c1dc7e2f854a4f6d4afe381596
povedaJ <jvpovedaorozco15@gmail.com>
2023-03-29 17:40:00 -0600

    Parte inicial del lab

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -3,7 +3,6 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
@@ -35,60 +34,31 @@ public class HelloController {
 
     @FXML
     void Home(ActionEvent event) {
-        this.txtMessage.setText("Laboratory No. 2");
+        this.txtMessage.setText("Laboratory No. 3");
         this.bp.setCenter(ap);
     }
 
     @FXML
-    void knapsackOnMousePressed(MouseEvent event) {
-        this.txtMessage.setText("Loading Knapsack Problem. Please wait!!!");
-        this.bp.setCenter(ap);
+    void divideandConquerOnaction(ActionEvent event) {
+        loadPage("divideandconquer.fml");
+
     }
 
     @FXML
-    void knapsackProblem(ActionEvent event) {
-        loadPage("KnapsackProblem.fxml");
+    void dynamicOnAction(ActionEvent event) {
+        loadPage("dinamic.fml");
+    }
+
+
+
+    @FXML
+    void probabilisticOnAction(ActionEvent event) {
+        loadPage("probabilisticOnAction.fml");
     }
 
     @FXML
-    void knightTour(ActionEvent event)  {loadPage("knightTour.fxml");}
+    void vectorOnAction(ActionEvent event) {
+        loadPage( "vectorOnAction.fml");
 
-    @FXML
-    void knightTourOnMousePressed(MouseEvent event) {
-        this.txtMessage.setText("Loading The Knight Tour. Please wait!!!");
-        this.bp.setCenter(ap);
-    }
-
-    @FXML
-    void nQueenOnMousePressed(MouseEvent event) {
-        this.txtMessage.setText("Loading N Queen Problem. Please wait!!!");
-        this.bp.setCenter(ap);
-    }
-
-    @FXML
-    void nQueenProblem(ActionEvent event) {
-        loadPage("nQueenProblem.fxml");
-    }
-
-    @FXML
-    void ratInMaze(ActionEvent event) {
-        loadPage("ratInMaze.fxml");
-    }
-
-    @FXML
-    void ratInMazeOnMousePressed(MouseEvent event) {
-        this.txtMessage.setText("Loading Rat in a Maze. Please wait!!!");
-        this.bp.setCenter(ap);
-    }
-
-    @FXML
-    void sudokuOnMousePressed(MouseEvent event) {
-        this.txtMessage.setText("Loading Sudoku Solver. Please wait!!!");
-        this.bp.setCenter(ap);
-    }
-
-    @FXML
-    void sudokuSolver(ActionEvent event) {
-        loadPage("sudokuSolver.fxml");
     }
 }
